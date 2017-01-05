5f16cede05ca54a0cff8c7b32733bae7df767c58
Joshua Lee <joshualee@iconfigure.org>
2017-01-04 21:55:19 -1000

    users can now GET index html file using "/"

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
 
+app.get('/', function (req, res) {
+  // res.writeHead(200, {'Content-type': 'text/html'});
+  console.log('the work was finished')
+   res.sendFile(__dirname + '/public/index.html');
+
+});
 
 
 var server = app.listen(3000, () => {
